dbf6f5922cb73441a6b6d9d490657005cdf0a765
Kirichenko14031991 <124730976+Kirichenko14031991@users.noreply.github.com>
2023-10-11 10:04:29 +0000

    Deploying to gh-pages from @ Kirichenko14031991/pokerist@df76d0b6ac2c214dbcf68e017a44be204b59f1bb 🚀

---
+++ index.04f1fe2f.js
@@ -0,0 +1,2 @@
+document.addEventListener("mousemove",(e=>{Object.assign(document.documentElement,{style:`\n    --move-x: ${-.005*(e.clientX-window.innerWidth/2)}deg;\n    --move-y: ${-.01*(e.clientY-window.innerHeight/2)}deg;\n    `})}));
+//# sourceMappingURL=index.04f1fe2f.js.map
---
+++ index.aa6110e6.js
@@ -0,0 +1,2 @@
+document.addEventListener("mousemove",(function(n){Object.assign(document.documentElement,{style:"\n    --move-x: ".concat(-.005*(n.clientX-window.innerWidth/2),"deg;\n    --move-y: ").concat(-.01*(n.clientY-window.innerHeight/2),"deg;\n    ")})}));
+//# sourceMappingURL=index.aa6110e6.js.map
